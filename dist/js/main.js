e943cd93a0c192c6834ff4579da6f6249fd5e07e
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 12:20:01 +0100

    We have created a new data.json with an array of presidentss. Then we create a XMLHttpRequest() object, and using xhr.open('GET', 'data.json', true); we indicate to prepare a statement with get method, to load file data.json, async. We force the xhr.responseType to be text to check if the data being read are the same as we expect. We do xhr.send() to init the operation. When load finishes we use xhr.onload to execute the function with checks if the load has been correct, the xhr.status === 200, which is OK; and then we parse the string as JSON and print it.

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,11 +3,12 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
 xhr.responseType = 'text';
+xhr.send();
+
 xhr.onload = function () {
     if (xhr.status === 200) {
         var object = JSON.parse(xhr.responseText);
         console.log(object);
     }
 };
-xhr.send();
 //# sourceMappingURL=main.js.map
--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
 xhr.responseType = 'text';
+xhr.send();
+
 xhr.onload = function () {
     if (xhr.status === 200) {
         var object = JSON.parse(xhr.responseText);
         console.log(object);
     }
 };
-xhr.send();
