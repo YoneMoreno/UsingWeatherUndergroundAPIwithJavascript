58e5cfda6b8c742c5c0c8fc277d8ba61d0e0e75f
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 12:06:32 +0100

    We learn how to use xhr.onload which is being triggered after the readyState chnges to 4, which means DONE; to get the data and parse it as JSON. In addition we saw how to enforce the data being read must be, with xhr.responseType = 'text'';

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,12 +2,12 @@
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
-
-xhr.send();
-
-xhr.onreadystatechange = function () {
-    console.log(xhr.readyState);
-    console.log(xhr.status);
-    console.log(xhr.statusText);
+xhr.responseType = 'text';
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var object = JSON.parse(xhr.responseText);
+        console.log(object);
+    }
 };
+xhr.send();
 //# sourceMappingURL=main.js.map
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET','data.json',true);
-
-xhr.send();
-
-xhr.onreadystatechange = function(){
-    console.log(xhr.readyState);
-    console.log(xhr.status);
-    console.log(xhr.statusText);
+xhr.open('GET', 'data.json', true);
+xhr.responseType = 'text';
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var object = JSON.parse(xhr.responseText);
+        console.log(object);
+    }
 };
+xhr.send();
