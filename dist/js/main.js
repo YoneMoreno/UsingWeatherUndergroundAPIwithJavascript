471128fb4442f484c0f7456d93d62841dc3f71e7
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 17:59:26 +0100

    We display current day and next 3 days forecast

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,6 +32,24 @@ weatherForecast.onload = function () {
         fObj = JSON.parse(weatherForecast.responseText);
         console.log(fObj);
         document.getElementById('desc').innerHTML = fObj.forecast.txt_forecast.forecastday["0"].fcttext_metric;
+        //Day 1
+
+        document.getElementById('r1c1').innerHTML = fObj.forecast.simpleforecast.forecastday[1].date.weekday;
+        document.getElementById('r1c2').src = fObj.forecast.simpleforecast.forecastday[1].icon_url;
+        document.getElementById('r1c3').innerHTML = fObj.forecast.simpleforecast.forecastday[1].high.celsius + "*C";
+        document.getElementById('r1c4').innerHTML = fObj.forecast.simpleforecast.forecastday[1].low.celsius + "*C";
+
+        //Day 2
+
+        document.getElementById('r2c1').innerHTML = fObj.forecast.simpleforecast.forecastday[2].date.weekday;
+        document.getElementById('r2c2').src = fObj.forecast.simpleforecast.forecastday[2].icon_url;
+        document.getElementById('r2c3').innerHTML = fObj.forecast.simpleforecast.forecastday[2].high.celsius + "*C";
+        document.getElementById('r2c4').innerHTML = fObj.forecast.simpleforecast.forecastday[2].low.celsius + "*C";
+        //Day 3
+        document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
+        document.getElementById('r3c2').src = fObj.forecast.simpleforecast.forecastday[3].icon_url;
+        document.getElementById('r3c3').innerHTML = fObj.forecast.simpleforecast.forecastday[3].high.celsius + "*C";
+        document.getElementById('r3c4').innerHTML = fObj.forecast.simpleforecast.forecastday[3].low.celsius + "*C";
     } //end if
 
 }; //end function
--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@ weatherConditions.open('GET', 'http://api.wunderground.com/api/9c4c0d8587580fce/
 weatherConditions.responseType = 'text';
 weatherConditions.send(null);
 
-weatherConditions.onload = function() {
-    if (weatherConditions.status === 200){
-        cObj = JSON.parse(weatherConditions.responseText); 
+weatherConditions.onload = function () {
+    if (weatherConditions.status === 200) {
+        cObj = JSON.parse(weatherConditions.responseText);
         console.log(cObj);
 
         document.getElementById('location').innerHTML = cObj.current_observation.display_location.full;
@@ -25,24 +25,33 @@ weatherConditions.onload = function() {
 weatherForecast.open('GET', 'http://api.wunderground.com/api/9c4c0d8587580fce/forecast/q/84653.json', true);
 
 
-
-
-
-
-
-
-
-
 weatherForecast.responseType = 'text';
 weatherForecast.send();
-weatherForecast.onload = function() {
+weatherForecast.onload = function () {
 
-    if (weatherForecast.status === 200){
+    if (weatherForecast.status === 200) {
         fObj = JSON.parse(weatherForecast.responseText);
         console.log(fObj);
         document.getElementById('desc').innerHTML = fObj.forecast.txt_forecast.forecastday["0"].fcttext_metric;
+        //Day 1
 
-} //end if
+        document.getElementById('r1c1').innerHTML = fObj.forecast.simpleforecast.forecastday[1].date.weekday;
+        document.getElementById('r1c2').src = fObj.forecast.simpleforecast.forecastday[1].icon_url;
+        document.getElementById('r1c3').innerHTML = fObj.forecast.simpleforecast.forecastday[1].high.celsius + "*C";
+        document.getElementById('r1c4').innerHTML = fObj.forecast.simpleforecast.forecastday[1].low.celsius + "*C";
+
+        //Day 2
+
+        document.getElementById('r2c1').innerHTML = fObj.forecast.simpleforecast.forecastday[2].date.weekday;
+        document.getElementById('r2c2').src = fObj.forecast.simpleforecast.forecastday[2].icon_url;
+        document.getElementById('r2c3').innerHTML = fObj.forecast.simpleforecast.forecastday[2].high.celsius + "*C";
+        document.getElementById('r2c4').innerHTML = fObj.forecast.simpleforecast.forecastday[2].low.celsius + "*C";
+        //Day 3
+        document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
+        document.getElementById('r3c2').src = fObj.forecast.simpleforecast.forecastday[3].icon_url;
+        document.getElementById('r3c3').innerHTML = fObj.forecast.simpleforecast.forecastday[3].high.celsius + "*C";
+        document.getElementById('r3c4').innerHTML = fObj.forecast.simpleforecast.forecastday[3].low.celsius + "*C";
+    } //end if
 
 
 }; //end function
