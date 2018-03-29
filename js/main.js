f9d3e32843c7724ec9e345f96c466610e0014b3d
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 12:45:59 +0100

    We are working with a JSON deep 2 and arrays.
    We append into HTML the president nama, last name and served years, and also the vicepresident fullname.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,29 @@ xhr.send();
 xhr.onload = function () {
     if (xhr.status === 200) {
         var object = JSON.parse(xhr.responseText);
-        console.log(object);
+        console.log(object.presidents);
+
+        for (var i = 0; i < object.presidents.length; i++) {
+            console.log(object.presidents[i].first);
+            console.log(object.presidents[i].last);
+            console.log(object.presidents[i].served);
+            console.log(object.vicepresidents[i].first);
+            console.log(object.vicepresidents[i].last);
+        }
+
+        var myText = "";
+        for (var i = 0; i < object.presidents.length; i++) {
+            var x = i + 1;
+            myText += '<br>President number '
+                + x + ' was: '
+                + object.presidents[i].first + '';
+            myText += ' Last name: '
+                + object.presidents[i].last + ' ';
+            myText += ' He served from ' + object.presidents[i].served + " with ";
+            myText += object.vicepresidents[i].first + ' ';
+            myText += object.vicepresidents[i].last;
+        }
+
+        document.getElementById('message').innerHTML = myText;
     }
 };
