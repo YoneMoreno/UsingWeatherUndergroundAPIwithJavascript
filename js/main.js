2dc9857dc12128c303efeaae52e51bbad42919c2
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 17:44:43 +0100

    We put the API forecast for today in our index.html

--- js/main.js
+++ js/main.js
@@ -21,27 +21,28 @@ weatherConditions.onload = function() {
         document.getElementById('temperature').innerHTML = cObj.current_observation.temp_c;
     } //end if
 }; //end function
-
-
-
-
-
-
-
-
-
-
 // GET THE FORECARST
 weatherForecast.open('GET', 'http://api.wunderground.com/api/9c4c0d8587580fce/forecast/q/84653.json', true);
-weatherForecast.responseType = 'text'; 
+
+
+
+
+
+
+
+
+
+
+weatherForecast.responseType = 'text';
 weatherForecast.send();
-
 weatherForecast.onload = function() {
-if (weatherForecast.status === 200){
-	fObj = JSON.parse(weatherForecast.responseText);
-	console.log(fObj);
-	
+
+    if (weatherForecast.status === 200){
+        fObj = JSON.parse(weatherForecast.responseText);
+        console.log(fObj);
+        document.getElementById('desc').innerHTML = fObj.forecast.txt_forecast.forecastday["0"].fcttext_metric;
+
 } //end if
+
+
 }; //end function
-
-
