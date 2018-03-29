5e9d73c1e03dc7d174bbabfa22c320cd31af3135
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 16:41:35 +0100

    init hospitality module, to load JSON data with two levels deep, and object/arrays inside of it.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,16 @@
+// JavaScript Document
+var hotelInfo;
+var details;
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'data.json', true);
+xhr.open('GET', "data.json", true);
 xhr.responseType = 'text';
 xhr.send();
 
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        var object = JSON.parse(xhr.responseText);
-        console.log(object.presidents);
 
-        for (var i = 0; i < object.presidents.length; i++) {
-            console.log(object.presidents[i].first);
-            console.log(object.presidents[i].last);
-            console.log(object.presidents[i].served);
-            console.log(object.vicepresidents[i].first);
-            console.log(object.vicepresidents[i].last);
-        }
-
-        var myText = "";
-        for (var i = 0; i < object.presidents.length; i++) {
-            var x = i + 1;
-            myText += '<br>President number '
-                + x + ' was: '
-                + object.presidents[i].first + '';
-            myText += ' Last name: '
-                + object.presidents[i].last + ' ';
-            myText += ' He served from ' + object.presidents[i].served + " with ";
-            myText += object.vicepresidents[i].first + ' ';
-            myText += object.vicepresidents[i].last;
-        }
-
-        document.getElementById('message').innerHTML = myText;
-    }
-};
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        hotelInfo = JSON.parse(xhr.responseText);
+        console.log(hotelInfo);
+  
+    } // end if
+} // end function
